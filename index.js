cf6e1e3ba7da2c0d1302ff6209e640a78ac68743
Vien Dinh <maxvien@gmail.com>
2021-07-09 16:04:26 +0700

    add prettier

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["react-app", "react-app/jest"],
+  extends: ["react-app", "react-app/jest", "prettier"],
   rules: {
     "react/react-in-jsx-scope": "off", // React is always in scope with Next
   },
